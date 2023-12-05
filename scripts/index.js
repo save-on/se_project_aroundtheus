0a766a18ca759d260e0bff3b5714a6b023cbd701
Sayvon <sedmon033@gmail.com>
2023-12-05 17:09:10 -0600

    modal js finished

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,16 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const page = document.querySelector(".page");
+const editBtn = page.querySelector(".profile__edit-btn");
+const modal = page.querySelector(".modal");
+const closeBtn = page.querySelector(".modal__close-btn");
+
+editBtn.addEventListener("click", function () {
+  modal.classList.add("modal_opened");
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("modal_opened");
+});
